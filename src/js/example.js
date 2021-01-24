048ec56746974c8e736a5d7c6b7e950e08aaa8f3
sp <sp@tiniyo.com>
2021-01-24 12:56:53 +0530

    chat popup

---
+++ src/js/example.js
@@ -0,0 +1,11 @@
+export const example = () => {
+  return 'Sensible webpack 5 boilerplate using Babel and PostCSS with a hot dev server and an optimized production build.'
+}
+
+export const openForm = () =>{
+  document.getElementById("myForm").style.display = "block";
+}
+
+export const closeForm = () =>{
+  document.getElementById("myForm").style.display = "none";
+}
